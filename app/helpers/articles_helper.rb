13dd93911e16577843194ddeecffbf47aa37823c
Andrew Baldwin <andrew.baldwin44@protonmail.com>
2020-04-06 00:16:50 -0400

    Added tagging

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,6 @@
 module ArticlesHelper
   private
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :tag_list)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,14 @@
 class Article < ApplicationRecord
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    self.tags.map { |tag| tag.name}.join(", ")
+  end
+
+  def tag_list=(tags_string)
+    self.tags = tags_string.split(", ").collect{ |s| s.downcase}.uniq
+                           .collect { |name| Tag.find_or_create_by(name: name)}
+  end
 end
